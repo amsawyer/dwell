356df38f467006d23867ac40f09055463b141dec
vagrant <vagrant@precise32.(none)>
2015-02-07 04:08:37 +0000

    change cities

--- cities.py
+++ cities.py
@@ -2,34 +2,32 @@ cities = { }
 def main():
 	myfile = open('cities.txt', 'r')
 	
-	i = 0;
+	i = 5;
 	counter = 0;
-	
-	print "hello"
 	for line in myfile:
-		print(i)
-		print(line)
 		if(i%5==0):
 			name = line
 			newCity = City(name)
-		if((i-1)%5==0):
+		elif((i-1)%5==0):
 			newCity.addSpr(line)
-		if((i-2)%5==0):
+		elif((i-2)%5==0):
 			newCity.addSumm(line)
-		if((i-3)%5==0):
+		elif((i-3)%5==0):
 			newCity.addAut(line)
-		if((i-3)%5==0):
+		elif((i-3)%5==0):
 			newCity.addWin(line)
-		addEntireCity(counter, newCity)
-		i = i+1
-		counter = counter + 1
+
+		if(i%5==0):
+			addEntireCity(counter, newCity)
+			counter = counter + 1
+		i = i + 1
 	
 	printCities()
 	return
 
 def printCities():
 	for i in range (0,100):
-		print str(cities[i].name) + "spr: " + str(cities[i].spr) + "\n"
+		print str(i) + " " + str(cities[i].name) + "spr: " + str(cities[i].spr) + "summ" + str(cities[i].summ) + "aut: " + str(cities[i].aut) + "win: " + str(cities[i].win)+"\n"
 	return
 
 def addEntireCity(city_id, cityObject):
@@ -40,19 +38,14 @@ class City:
 	""" Object for modelling a city """
 	def __init__(self,name):
 		self.name = name
-		return	
 	def addSpr(self,spr):
 		self.spr = spr
-		return
 	def addSumm(self,summ):
 		self.summ = summ
-		return
 	def addAut(self,aut):
 		self.aut = aut
-		return
 	def addWin(self,win):
 		self.win = win
-		return
 		
 if __name__ == "__main__":
 	main()
