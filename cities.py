9e7ed402a2e7c5e9c535ba76ba0d06e00703d3b3
Amanda Song <amandasong@users.noreply.github.com>
2015-02-06 23:18:09 -0500

    Update cities.py

--- cities.py
+++ cities.py
@@ -1,59 +1,42 @@
 cities = { }
 def main():
-	myfile = open('cities.txt', 'r')
-	
-	i = 0;
-	counter = 0;
-	
-	print "hello"
-	for line in myfile:
-		print(i)
-		print(line)
-		if(i%5==0):
-			name = line
-			newCity = City(name)
-		if((i-1)%5==0):
-			newCity.addSpr(line)
-		if((i-2)%5==0):
-			newCity.addSumm(line)
-		if((i-3)%5==0):
-			newCity.addAut(line)
-		if((i-3)%5==0):
-			newCity.addWin(line)
-		addEntireCity(counter, newCity)
-		i = i+1
-		counter = counter + 1
-	
-	printCities()
-	return
+    myfile = open('cities.txt', 'r')
+    
+    i = 0
+    counter = 0
+    
+    while (i < 500):
+        name = myfile.readline()
+        spr = myfile.readline()
+        summ = myfile.readline()
+        aut = myfile.readline()
+        win = myfile.readline()
+        newCity = City(name, spr, summ, aut, win)
+        addEntireCity(counter, newCity)
+        i += 5
+        counter += 1
+    
+    printCities()
+    return
 
 def printCities():
-	for i in range (0,100):
-		print str(cities[i].name) + "spr: " + str(cities[i].spr) + "\n"
-	return
+    for i in range (0,100):
+        print str(cities[i].name) + "spring: " + str(cities[i].spr) + "summer: " + str(cities[i].summ) + "autumn: " + str(cities[i].aut) + "winter: " + str(cities[i].win)
+    return
 
 def addEntireCity(city_id, cityObject):
-	cities[city_id] = cityObject
-	return
+    cities[city_id] = cityObject
+    return
 
 class City:
-	""" Object for modelling a city """
-	def __init__(self,name):
-		self.name = name
-		return	
-	def addSpr(self,spr):
-		self.spr = spr
-		return
-	def addSumm(self,summ):
-		self.summ = summ
-		return
-	def addAut(self,aut):
-		self.aut = aut
-		return
-	def addWin(self,win):
-		self.win = win
-		return
-		
+    """ Object for modelling a city """
+    def __init__(self, name, spr, summ, aut, win):
+        self.name = name
+        self.spr = spr
+        self.summ = summ
+        self.aut = aut
+        self.win = win
+        return
+        
 if __name__ == "__main__":
-	main()
-
+    main()
