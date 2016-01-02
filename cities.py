f82ad639520b680ab507e03e0b61cb89cf7088f9
Ali Sawyer <allisonmarysawyer@gmail.com>
2016-01-02 16:20:08 +0100

    Updated to match branch 'ali' on amandasong/dwell

--- cities.py
+++ cities.py
@@ -1,7 +1,11 @@
+
+m flask import Flask, jsonify
 import math
+import operator
+import requests
 
 cities = { }
-def main():
+def average(usr_spr, usr_summ, usr_aut, usr_win):
     myfile = open('cities.txt', 'r')
     
     i = 0
@@ -18,14 +22,17 @@ def main():
         i += 5
         counter += 1
     
-    printCities()
-    usr_spr = 60
-    usr_summ = 75
-    usr_aut = 60
-    usr_win = 40
-    
-    calc(usr_spr, usr_summ, usr_aut, usr_win)
-    return
+    tup = calc(usr_spr, usr_summ, usr_aut, usr_win) # full 100
+    sorted_tup = sort(tup) # sorted 100
+    city_list = []
+    for i in range (0, 3):
+        one_tup = sorted_tup[i]
+        city_only = one_tup[0]
+        city_list.append(city_only[:-1]) # list of just cities, no scores
+
+    #photos = get_photos(city_list)
+
+    return city_list
 
 def printCities():
     for i in range (0,100):
@@ -37,24 +44,57 @@ def addEntireCity(city_id, cityObject):
     return
 
 def calc(usr_spr, usr_summ, usr_aut, usr_win):
-	for i in range(0, 100):
-		spr = cities[i].spr
-		summ = cities[i].summ
-		aut = cities[i].aut
-		win = cities[i].win
+    name_list = []
+    score_list = []
+    for i in range(0, 100):
+        spr = cities[i].spr
+        summ = cities[i].summ
+        aut = cities[i].aut
+        win = cities[i].win
 
-		dif1sq = math.pow(abs(usr_spr - float(spr)),2)
-		dif2sq = math.pow(abs(usr_summ - float(summ)),2)
-		dif3sq = math.pow(abs(usr_aut - float(aut)),2)
-		dif4sq = math.pow(abs(usr_win - float(win)),2)
-		
-		total = dif1sq + dif2sq + dif3sq + dif4sq
-		score = math.sqrt(total)
-		print str(i) + "City name: " + str(cities[i].name) + "score: " + str(score)
-	return
+        dif1sq = math.pow(abs(float(usr_spr) - float(spr)),2)
+        dif2sq = math.pow(abs(float(usr_summ) - float(summ)),2)
+        dif3sq = math.pow(abs(float(usr_aut) - float(aut)),2)
+        dif4sq = math.pow(abs(float(usr_win) - float(win)),2)
+        
+        total = dif1sq + dif2sq + dif3sq + dif4sq
+        score = math.sqrt(total)
+
+        # Add to lists
+        name_list.append(str(cities[i].name))
+        score_list.append(score)
+        # print str(i) + "City name: " + str(cities[i].name) + "score: " + str(score)
+
+    # Create 2-tuple of names and scores
+    tup = zip(name_list, score_list)
+    return tup
+
+def sort(tup):
+    tup.sort(key=operator.itemgetter(1))
+    return tup
+
+'''
+def get_photos(city_list):
+    photo_urls = []
+    for i in range (0, 3):
+        url = "https://api.shutterstock.com/v2/images/search?per_page=1&query=" + city_list[i]
+        # necessary URL formatting modifications
+        url = url.replace(" ", "+")
+        comma_idx = url.rfind(",")
+        url = url[0:comma_idx]
+        print url
+        response = requests.get(url)
+        response_dict = response.json()
+        photo_url = (response_dict.get("large_thumb")).get("url")
+        print photo_url
+        photo_urls.append(photo_url)
+       
+    print photo_urls
+    return photo_urls
+'''
 
 class City:
-    """ Object for modelling a city """
+    ''' Object for modelling a city '''
     def __init__(self, name, spr, summ, aut, win):
         self.name = name
         self.spr = spr
@@ -64,4 +104,4 @@ class City:
         return
         
 if __name__ == "__main__":
-	main()
+    main()
--- dwell.py
+++ dwell.py
@@ -1,36 +1,47 @@
 from flask import Flask, jsonify, render_template, request
-
 import requests
+import cities
+# Configuration
 
 app = Flask(__name__)
-
+app.config.from_object(__name__)
 
 app.config['DEBUG'] = True
 
+# app routes
+
 @app.route("/")
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
 
+## is this route unnecessary?
 @app.route("/search", methods=["GET", "POST"])
 def search():
-	if request.method=="POST":
-		return render_template("results.html", api_data=response_dict)
-	else:
-		return render_template("search.html")
+    if request.method=="POST":
+        return render_template("results.html", api_data=response_dict)
+    else:
+        return render_template("search.html")
 
-@app.route("/results",methods=["GET","POST"]}
+@app.route("/results", methods=["GET", "POST"]) 
 def get_results():
     if request.method == "POST":
         spr_temp = request.form["input_spr"]
         summ_temp = request.form["input_summ"]
         aut_temp = request.form["input_aut"]
         win_temp = request.form["input_win"]
-        
+        print spr_temp
+        print summ_temp
+        print aut_temp
+        print win_temp
         return render_template("results.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.errorhandler(404)
 def notfound(error):
-	return "Sorry, that page does not exist.", 404
+    return "Sorry, that page does not exist.", 404
 
-if __name__ = '__main__':
-	app.run(host = '0.0.0.0')
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
